055f3a4a4c755f65e0197ea3acfa456323166574
theoremoon <theoldmoon0602@gmail.com>
2020-11-26 16:13:47 +0900

    update

--- ansible/run.bash
+++ ansible/run.bash
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -ex
 
-ansible-vault decrypt ssh.key.vault --vault-password-file vault-key.sh --output ssh.key
-ansible-vault decrypt aws.yml.vault --vault-password-file vault-key.sh --output aws.yml
+bash ./vault-encrypt.bash decrypt
 trap 'rm ssh.key; rm aws.yml' 0 1 2 3 15
 
 export ANSIBLE_BECOME_PASS=some_password_here
